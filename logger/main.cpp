dc9c48f964006486d347756cb0259dcb1ba24e2a
Illia Chaparyn <55086550+ewoxej@users.noreply.github.com>
2021-07-24 10:48:25 +0300

    Initial Commit

---
+++ logger/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  logger
+//
+//  Created by Illia Chaparyn on 24.07.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
